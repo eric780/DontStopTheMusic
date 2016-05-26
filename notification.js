39c7efd18e83bdeb1ad052de220ab472c14f809d
Eric Lin <ericsuhelin@gmail.com>
2016-05-26 12:43:28 -0400

    changed to use sessionstorage instead of saving a div

--- notification.js
+++ notification.js
@@ -1,21 +1,11 @@
 console.log('notification script injected to page'); 
 
 
-// insert flag
-var flag = document.getElementById("audibleGlobal780123");
-if (!flag) {
-    var temp = document.createElement('div');
-    temp.id = 'audibleGlobal780123';
-    document.body.appendChild(temp);
-    console.log('appended')
-}
 
 
-var audible = true;
+sessionStorage.setItem('audible', true);
 window.onbeforeunload = function(){ 
-
-    var flag = document.getElementById("audibleGlobal780123");
-    if (flag) {
+    if (sessionStorage.getItem('audible')) {
         return "Are you sure you want to close your music?"
     }
 
--- removeNotification.js
+++ removeNotification.js
@@ -1,8 +1,4 @@
 console.log("no longer audible");
-audible = false;
 
-var flag = document.getElementById("audibleGlobal780123");
-if (flag) {
-    flag.parentNode.removeChild(flag);
-}
-console.log('removed')
+sessionStorage.removeItem('audible');
+console.log('removed');
